dfb765169c5ee3af1ab0a3d9f63ab9ef5dad27b7
myersg86 <myersg86@gmail.com>
2018-08-02 12:39:29 -0600

    updates

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-# Install dependencies & recommendations
-sudo dpkg --configure --pending &&
+# Configure dpkg & install apt utils
 sudo dpkg --configure -a &&
-sudo apt-get install apt aptitude apt-listchanges &&
 sudo apt-get update &&
-sudo apt update &&
-sudo apt install -f &&
-sudo apt-get install --install-recommends -y aptitude git git-core software-properties-common dirmngr vim python-apt
-
-# Install ansible
-if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Adding Ansible keys "
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 &&
-    echo "Ansible keys added."
-    echo ""
-    echo "Adding latest Ansible repo to /etc/apt/sources.list "
-    sudo sh -c "echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list"
-fi
-
-if ! hash ansible >/dev/null 2>&1; then
-    echo "Installing Ansible..."#!/bin/bash
-
 # Install dependencies & recommendations
-sudo apt update
-sudo apt-get install --install-recommends -y git git-core software-properties-common dirmngr vim aptitude python-apt
+sudo apt-get install --install-recommends -y apt aptitude git git-core software-properties-common dirmngr vim python-apt &&
 
 # Install ansible
 if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
